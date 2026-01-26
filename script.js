1dda63e95fb320061401b05d4d254f6152c007c9
Juan <juanfelix.rj@gmail.com>
2026-01-26 14:09:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const toogleBTN = document.querySelector("#password-toogle");
+const passwordInput = document.querySelector("#password-input");
+const toogleConfirmBTN = document.querySelector("#password-confirm-toogle");
+const passwordConfirmInput = document.querySelector("#password-confirm-input");
+
+const submitBTN = document.querySelector("#submit");
+
+toogleBTN.addEventListener("click", () =>{
+    if(passwordInput.type == "password"){
+        passwordInput.type = "text";
+        toogleBTN.innerHTML = '<i class="bi bi-eye-slash-fill"></i>';
+    }else{
+        passwordInput.type = "password";
+        toogleBTN.innerHTML = '<i class="bi bi-eye-fill"></i>';
+    }
+});
+
+toogleConfirmBTN.addEventListener("click", () =>{
+    if(passwordConfirmInput.type == "password"){
+        passwordConfirmInput.type = "text";
+        toogleConfirmBTN.innerHTML = '<i class="bi bi-eye-slash-fill"></i>';
+    }else{
+        passwordConfirmInput.type = "password";
+        toogleConfirmBTN.innerHTML = '<i class="bi bi-eye-fill"></i>';
+    }
+});
+
+submitBTN.addEventListener("click", () =>{
+    if(passwordInput.value !== passwordConfirmInput.value){
+        alert("Passwords do not match!");
+    }else{
+        alert("Registered successfully!");
+    }
+});
